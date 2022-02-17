15ac424af7754c1c2dc92f699362fbc2d54e2079
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-02-17 22:25:03 +0530

    Solution for finding number of notebooks

---
+++ codechef/NOTEBOOK.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    n=n*10;
+	    cout<<n<<endl;
+	}
+	
+	return 0;
+}
