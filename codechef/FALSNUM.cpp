3778f03869a6a47eb9142c0369839bb08d2202ad
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-02-17 23:35:27 +0530

    Finding the smallest possible number

---
+++ codechef/FALSNUM.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    string s;
+	    cin>>s;
+	    
+	    if(s[0]=='1')
+	    {
+	        s="10"+s.substr(1,s.length());
+	    }else{
+	        s="1"+s;
+	    }
+	    cout<<s<<endl;
+	}
+	
+	return 0;
+}
