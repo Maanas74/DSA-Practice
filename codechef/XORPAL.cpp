9960b68db1f5ba4ac0e08c1c3862fadf6a8a8982
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-02-17 18:53:45 +0530

    Solution for XOR Palindrome

---
+++ codechef/XORPAL.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    string s;
+	    cin>>s;
+	    
+	    if(n==2 || n%2!=0)
+	    {
+	        cout<<"YES"<<endl;
+	    }else{
+	        
+	        int c=0,d=0;
+	        
+	        for(int i=0;i<n;i++)
+	        {
+	            if(s[i]=='0')
+	            {
+	                c++;
+	            }else{
+	                d++;
+	            }
+	        }
+	        
+	        if(c==d || (c%2==0 && d%2==0)){
+	            cout<<"YES"<<endl;
+	        }else{
+	            cout<<"NO"<<endl;
+	        }
+	    }
+	    
+	}
+	
+	return 0;
+}
