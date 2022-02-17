098bc9e88870dcd8606c75551efd81fcdaec2e73
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-02-17 23:48:31 +0530

    Finding number of good pairs

---
+++ codechef/numberofgoodpairs.cpp
@@ -0,0 +1,20 @@
+// Que link:-- https://leetcode.com/problems/number-of-good-pairs/submissions/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int c=0;
+        
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+};
