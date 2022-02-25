3b1273455e0d2b26cf86cb4bf90554e69f84afcd
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-02-25 22:48:51 +0530

    Solving 100-x

---
+++ codechef/SHOPCHANGE.cpp
@@ -0,0 +1,22 @@
+// Que Link:- https://www.codechef.com/problems/SHOPCHANGE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    cout<<100-x<<endl;
+	    
+	}
+	
+	return 0;
+}
