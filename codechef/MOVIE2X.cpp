09c3956fb83c7831fa24c90e4e6b94c7169e2606
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-03-01 21:36:02 +0530

    Finding total minutes watched

---
+++ codechef/MOVIE2X.cpp
@@ -0,0 +1,18 @@
+// Que Link:- https://www.codechef.com/LTIME105C/problems/MOVIE2X
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int x,y;
+	cin>>x>>y;
+	
+	y=y/2;
+	
+	cout<<x-y<<endl;
+	
+	return 0;
+}
