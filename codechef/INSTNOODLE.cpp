ad17b0a36de3a308f7cae1ba3ad82b682fcbffea
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-03-04 23:12:17 +0530

    multiply x*y

---
+++ codechef/INSTNOODLE.cpp
@@ -0,0 +1,16 @@
+// Que Link:- https://www.codechef.com/problems/INSTNOODLE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int x,y;
+	cin>>x>>y;
+	
+	cout<<x*y<<endl;
+	
+	return 0;
+}
