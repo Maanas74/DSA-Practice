97af57577b356c3b0fac7a35b2a2f15ccf598442
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-03-08 22:42:29 +0530

    Final prices after discount on items

---
+++ LeetCode/Price-with-Discount.cpp
@@ -0,0 +1,23 @@
+// Que Link:- https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+            
+        for(int i=0;i<prices.size();i++)
+        {
+            for(int j=i+1;j<prices.size();j++)
+            {
+                if(prices[i]>=prices[j])
+                {
+                    prices[i]-=prices[j];
+                    break;
+                }
+            }
+        }
+        
+        return prices;
+        
+        
+    }
+};
