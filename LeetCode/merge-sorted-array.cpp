6e7731b017c42b272b1795fc1fb2906c63416932
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-03-11 19:34:25 +0530

    merging two sorted arrays

---
+++ LeetCode/merge-sorted-array.cpp
@@ -0,0 +1,44 @@
+// Que Link:- https://leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int i=0,j=0;
+        
+        vector<int> a;
+        
+        while(i<m && j<n)
+        {
+            if(nums1[i]<=nums2[j])
+            {
+                a.push_back(nums1[i]);
+                i++;
+            }else{
+                a.push_back(nums2[j]);
+                j++;
+            }
+        }
+        
+        if(i<m)
+        {
+            while(i<m)
+            {
+                a.push_back(nums1[i]);
+                i++;
+            }
+        }
+        
+        if(j<n)
+        {
+            while(j<n)
+            {
+                a.push_back(nums2[j]);
+                j++;
+            }
+        }
+        
+        nums1=a;
+        
+    }
+};
