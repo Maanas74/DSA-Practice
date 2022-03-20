6dbdb3c4582aef480dec6a5195136031c1d203ba
Maanas <55449222+Maanas74@users.noreply.github.com>
2022-03-20 22:17:49 +0530

    Finding how much tank we can filled

---
+++ codechef/FBC.cpp
@@ -0,0 +1,21 @@
+// Que Link:- https://www.codechef.com/problems/FBC
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int x,k;
+	    cin>>x>>k;
+	    
+	    cout<<x-k<<endl;
+	}
+	
+	return 0;
+}
